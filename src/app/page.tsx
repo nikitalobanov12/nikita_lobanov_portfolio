704848c30ae0ca50c9ff9c6ff14bb10655217e10
Nikita Lobanov <nikitaalobanovv@gmail.com>
2024-11-22 20:27:18 -0800

    added work experience

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import About from "./components/about";
+import Experience from "./components/experience";
 import Hero from "./components/hero";
 import Projects from "./components/projects";
 
@@ -12,6 +13,7 @@ export default function Home() {
       <Hero/>
       <About/>
       <Projects/>
+      <Experience/>
     </main>
     
   )
