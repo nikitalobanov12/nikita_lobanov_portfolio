4da39326d66ad1a7ca7a4907759455ab2977fa0e
nikitalobanov12 <nikitaalobanovv@gmail.com>
2025-05-30 22:30:42 -0700

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,37 @@
-import React from "react"; 
-import type { Metadata } from "next";
-import "./globals.css";
-import { Inter as FontSans } from "next/font/google"
-import { cn } from "@/lib/utils"
+import React from 'react';
+import type { Metadata } from 'next';
+import './globals.css';
+import { Inter as FontSans } from 'next/font/google';
+import { cn } from '@/lib/utils';
+import { Analytics } from '@vercel/analytics/next';
 
-const fontSans = FontSans( {
-	subsets: ["latin"],
-	variable: "--font-sans",
-})
+const fontSans = FontSans({
+	subsets: ['latin'],
+	variable: '--font-sans',
+});
 
 export const metadata: Metadata = {
-  title: "Nikita Lobanov | Full-Stack Web Developer",
-  description: "Full-Stack Web Developer portfolio showcasing projects and experience",
+	title: 'Nikita Lobanov | Full-Stack Web Developer',
+	description: 'Full-Stack Web Developer portfolio showcasing projects and experience',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-		<body className={cn(
-			"min-h-screen bg-background font-sans antialiased",
-			fontSans.variable
-		)}>
-			<div className="relative flex min-h-screen flex-col">
-				{children}
-			</div>
-			<div id="mobile-menu-root"></div>
-		</body>
-    </html>
-  );
+	return (
+		<html
+			lang='en'
+			suppressHydrationWarning
+		>
+			<body className={cn('min-h-screen bg-background font-sans antialiased', fontSans.variable)}>
+				<div className='relative flex min-h-screen flex-col'>
+					{children}
+					<Analytics />
+				</div>
+				<div id='mobile-menu-root'></div>
+			</body>
+		</html>
+	);
 }
